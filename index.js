472c58acb2e59604b6c5724475cd823f70a3b96f
albin <albinp2009@live.se>
2022-02-03 20:14:33 +0100

    added cors

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const app = express();
 const windparks = require("./routes/windparks");
 const users = require("./routes/users");
 const auth = require("./routes/auth");
+const cors = require("cors");
 
 dotenv.config();
 
 if (!process.env.JWT_SECRET) {
-  console.error('ERROR: JWT Secret not set');
+  console.error("ERROR: JWT Secret not set");
   process.exit(1);
 }
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/windparks", windparks);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
