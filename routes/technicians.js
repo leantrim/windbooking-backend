39d24bc8f034d85bf9d3cdee8093c0b38b364a1e
Leutrim Bunjaku <leutrimbunjaku5@gmail.com>
2022-02-17 19:06:01 +0100

    profile for technician

--- routes/technicians.js
+++ routes/technicians.js
@@ -3,19 +3,19 @@ const auth = require('../middleware/auth');
 const { Technician, validate } = require('../models/Technicians');
 const router = express.Router();
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async(req, res) => {
     console.log(req.params.id);
     if (!req.params.id) return res.status(400).send('userID is required');
     const technician = await Technician.findOne({ userID: req.params.id });
     return res.send(technician);
 })
 
-router.get("/", async (req, res) => {
+router.get("/", async(req, res) => {
     const technicians = await Technician.find();
     return res.send(technicians);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', async(req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.message);
 
@@ -30,26 +30,23 @@ router.post('/', async (req, res) => {
     return res.send(technician);
 })
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', async(req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.message);
 
     const technician = await Technician.findOne({ userID: req.params.id });
     if (!technician) return res.status(400).send('Invalid technician userID');
 
-    const tech = await Technician.findOneAndUpdate(
-        { userID: req.params.id },
-        {
-            workPermit: req.body.workPermit,
-            driverLicense: req.body.driverLicense,
-            tools: req.body.tools,
-            certifications: req.body.certifications,
-            achievements: req.body.achievements,
-            transportVehicle: {
-                winterTyres: req.body.winterTyres,
-            }
-        },
-    );
+    const tech = await Technician.findOneAndUpdate({ userID: req.params.id }, {
+        workPermit: req.body.workPermit,
+        driverLicense: req.body.driverLicense,
+        tools: req.body.tools,
+        certifications: req.body.certifications,
+        achievements: req.body.achievements,
+        transportVehicle: {
+            winterTyres: req.body.winterTyres,
+        }
+    }, );
 
 
     return res.send(tech);
