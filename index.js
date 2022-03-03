1659bf130f167981f6fbc5a2e07f82b87be90edf
Leutrim Bunjaku <leutrimbunjaku5@gmail.com>
2022-03-03 18:10:47 +0100

    adjusted windfarm to windparks

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
-const windfarms = require("./routes/windfarms");
+const windfarms = require("./routes/windParks");
 const users = require("./routes/users");
 const technicians = require("./routes/technicians");
 const auth = require("./routes/auth");
@@ -11,8 +11,8 @@ const cors = require("cors");
 dotenv.config();
 
 if (!process.env.JWT_SECRET) {
-  console.error("ERROR: JWT Secret not set");
-  process.exit(1);
+    console.error("ERROR: JWT Secret not set");
+    process.exit(1);
 }
 
 app.use(express.json());
@@ -23,10 +23,10 @@ app.use("/api/technicians", technicians);
 app.use("/api/auth", auth);
 
 app.listen(5000, () => {
-  console.log("listening on port 5000...");
+    console.log("listening on port 5000...");
 });
 
 mongoose
-  .connect("mongodb://localhost/windbooking-db")
-  .then(() => console.log("connected to MongoDB..."))
-  .catch((error) => console.log("Could not connect...", error));
+    .connect("mongodb://localhost/windbooking-db")
+    .then(() => console.log("connected to MongoDB..."))
+    .catch((error) => console.log("Could not connect...", error));
