3e2e977d5181748bc89878113d3d6c0b80219bec
Catarina Persson <catarina.persson@yahoo.se>
2022-03-06 10:33:23 +0100

    added name to address and changed joischema with date instead of string

--- models/WindFarms.js
+++ models/WindFarms.js
@@ -6,6 +6,7 @@ const Windfarm = mongoose.model(
   "Windfarm",
   new mongoose.Schema({
     address: {
+      name: { type: String, minlength: 2, maxlength: 100, required: true },
       street: { type: String, minlength: 2, maxlength: 50, required: true },
       zipcode: { type: String, minlength: 5, maxlength: 5, required: true },
       city: { type: String, minlength: 2, maxlength: 100, required: true },
@@ -19,7 +20,7 @@ const Windfarm = mongoose.model(
     windFarmDetails: {
       hubHeight: { type: String, required: true },
       elevatorType: { type: String, required: true },
-      commissioningDate: { type: String, required: true },
+      commissioningDate: { type: Date, required: true },
       safetyEquipment: { type: String, required: true },
       numberOfWindTurbines: { type: String, requrired: true },
     },
@@ -29,6 +30,7 @@ const Windfarm = mongoose.model(
 function validateWindfarm(windfarm) {
   const schema = Joi.object({
     address: Joi.object({
+      name: Joi.string().required().min(2).max(100),
       street: Joi.string().required().min(2).max(50),
       zipcode: Joi.string().required().min(5).max(5),
       city: Joi.string().required().min(2).max(100),
@@ -42,7 +44,7 @@ function validateWindfarm(windfarm) {
     windFarmDetails: Joi.object({
       hubHeight: Joi.string().required(),
       elevatorType: Joi.string().required(),
-      commissioningDate: Joi.string().required(),
+      commissioningDate: Joi.date().required(),
       safetyEquipment: Joi.string().required(),
       numberOfWindTurbines: Joi.string().required(),
     }),
